0c12c4403681e9038e2a4dcf510f97b899361887
AntoineSb <antoine.sb@orange.fr>
2018-03-01 20:52:56 +0000

    Bug correction

--- HashCode2018/intersection.hpp
+++ HashCode2018/intersection.hpp
@@ -6,10 +6,10 @@
 
 typedef std::pair<unsigned int, unsigned int> intersection;
 
-extern std::ostream& operator<<(std::ostream& os, const intersection& inter) {
+static std::ostream& operator<<(std::ostream& os, const intersection& inter) {
 	return os << inter.first << ':' << inter.second;
 }
 
-extern intersection operator-(const intersection &inter1, const intersection &inter2) {
+static intersection operator-(const intersection &inter1, const intersection &inter2) {
 	return intersection(abs((long)(inter1.first - inter2.first)), abs((long)(inter1.second - inter2.second)));
 }
