eab7700131f296c2f66539eb1d41a14be5182f68
AntoineSb <antoine.sb@orange.fr>
2018-03-01 20:13:19 +0000

    Ajout de l'opérateur de soustraction pour les intersections

--- HashCode2018/main.hpp
+++ HashCode2018/main.hpp
@@ -3,9 +3,14 @@
 #include <vector>
 #include <utility>
 #include <iostream>
+#include <cstdlib>
 
 typedef std::pair<unsigned int, unsigned int> intersection;
 
 std::ostream& operator<<(std::ostream& os, const intersection& inter) {
 	os << inter.first << ':' << inter.second;
+}
+
+intersection operator-(const intersection &inter1, const intersection &inter2) {
+	return intersection(abs((long)(inter1.first - inter2.first)), abs((long)(inter1.second - inter2.second)));
 }
